bd61ecd683bb4306d04c789e84a19a136de795a2
Patrick Perkins <plp248@nyu.edu>
2022-09-11 20:17:49 -0400

    more intentional quote usage - only use doubles when interpolating

--- unified_search.rb
+++ unified_search.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'json'
 
 # initial variable setup
-puts "Enter your search query: "
+puts 'Enter your search query: '
 search_query = gets.chomp
 colenda_url = "https://colenda.library.upenn.edu/?utf8=%E2%9C%93&search_field=all_fields&format=json&q=#{search_query}"
 finding_aids_url = "https://findingaids.library.upenn.edu/records?f[record_source][]=upenn&format=json&q=#{search_query}"
@@ -16,7 +16,7 @@ finding_aids_uri = URI finding_aids_url
 finding_aids_response = JSON.parse Net::HTTP.get(finding_aids_uri)
 
 # grab relevant search result data from colenda
-colenda_response_items = colenda_response["response"]["docs"]
+colenda_response_items = colenda_response['response']['docs']
 
 # extract the earliest year that appears in the input date string
 def extract_date date_string
@@ -32,29 +32,29 @@ end
 # create empty hash, iterate through colenda search result data, and add extracted data into hash
 colenda_constructed_hash = {}
 colenda_response_items.each do |r|
-  colenda_constructed_hash[r["id"]] = {
-    title:    r["title_tesim"].nil? ? "" : r["title_tesim"], # keep it as an array, don't want to lose information and have different data types
-    subjects: r["subject_tesim"].nil? ? "" : r["subject_tesim"],
-    abstract: r["abstract_tesim"].nil? ? "" : r["abstract_tesim"], # some values are nil - we want to have only strings
-    date:     r["date_tesim"].nil? ? "" : extract_date(r["date_tesim"].join ", "),
-    link:     "https://colenda.library.upenn.edu/catalog/#{r["id"]}",
-    source:   "Colenda"
+  colenda_constructed_hash[r['id']] = {
+    title:    r['title_tesim'].nil? ? '' : r['title_tesim'], # keep it as an array, don't want to lose information and have different data types
+    subjects: r['subject_tesim'].nil? ? '' : r['subject_tesim'],
+    abstract: r['abstract_tesim'].nil? ? '' : r['abstract_tesim'], # some values are nil - we want to have only strings
+    date:     r['date_tesim'].nil? ? '' : extract_date(r['date_tesim'].join ', '),
+    link:     "https://colenda.library.upenn.edu/catalog/#{r['id']}",
+    source:   'Colenda'
   }
 end
 
 # grab relevant search result data from finding aids
-finding_aids_response_items = finding_aids_response["data"]
+finding_aids_response_items = finding_aids_response['data']
 
 # create empty hash, iterate through finding aids search result data, and add extracted data into hash
 # these values must be converted to arrays in order to preserve the array data type in the colenda data
 finding_aids_constructed_hash = {}
 finding_aids_response_items.each do |r|
-  finding_aids_constructed_hash[r["id"]] = {
-    title:    r["attributes"]["title_tsi"].nil? ? "" : [r["attributes"]["title_tsi"]["attributes"]["value"]],
-    subjects: r["attributes"]["subjects_ssim"].nil? ? "" : [r["attributes"]["subjects_ssim"]["attributes"]["value"]],
-    abstract: r["attributes"]["abstract_scope_contents_tsi"].nil? ? "" : [r["attributes"]["abstract_scope_contents_tsi"]["attributes"]["value"]],
-    date:     r["attributes"]["display_date_ssim"].nil? ? "" : extract_date(r["attributes"]["display_date_ssim"]["attributes"]["value"]),
-    link:     r["attributes"]["title_tsi"]["id"],
+  finding_aids_constructed_hash[r['id']] = {
+    title:    r['attributes']['title_tsi'].nil? ? '' : [r['attributes']['title_tsi']['attributes']['value']],
+    subjects: r['attributes']['subjects_ssim'].nil? ? '' : [r['attributes']['subjects_ssim']['attributes']['value']],
+    abstract: r['attributes']['abstract_scope_contents_tsi'].nil? ? '' : [r['attributes']['abstract_scope_contents_tsi']['attributes']['value']],
+    date:     r['attributes']['display_date_ssim'].nil? ? '' : extract_date(r['attributes']['display_date_ssim']['attributes']['value']),
+    link:     r['attributes']['title_tsi']['id'],
     source:   'Finding Aids'
   }
 end
